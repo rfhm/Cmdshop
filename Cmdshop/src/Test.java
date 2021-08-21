956d20247945ba25f52ab227bad41810d1eed11d
rfhm <1981897922@qq.com>
2021-08-21 18:27:14 +0800

    用户登录失败后，让其重新登录

--- Cmdshop/src/Test.java
+++ Cmdshop/src/Test.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws ClassNotFoundException {
-        //File file=new File("D:\\JAVAlx\\shangcheng\\src\\users.xlsx");
-        InputStream in=Class.forName("Test").getResourceAsStream("/users.xlsx");
-        ReadExcel readExcel=new ReadExcel();
-        User[] users=readExcel.readExcel(in);
+        boolean bo=true;
+        while (bo) {
+            //File file=new File("D:\\JAVAlx\\shangcheng\\src\\users.xlsx");
+            InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
+            ReadExcel readExcel = new ReadExcel();
+            User[] users = readExcel.readExcel(in);
 
-        System.out.println("请输入用户名");
-        Scanner sc=new Scanner(System.in);
-        String username=sc.next();
+            System.out.println("请输入用户名");
+            Scanner sc = new Scanner(System.in);
+            String username = sc.next();
 
-        System.out.println("请输入密码");
-        String password=sc.next();
+            System.out.println("请输入密码");
+            String password = sc.next();
 
-        for(User user:users){
-            if(username.equals(user.getUsername())&&password.equals(user.getPassword())){
-                System.out.println("登录成功");
-                break;
-            }else{
-                System.out.println("用户名或者密码错误");
-            }
+            for (User user : users) {
+                if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
+                    System.out.println("登录成功");
+                    bo=false;
+                    break;
+                } else {
+                    System.out.println("用户名或者密码错误");
+                    break;
+                }
              /*for(User user:users){
             System.out.println(user.getUsername());
             System.out.println(user.getPassword());
@@ -30,6 +34,7 @@ public class Test {
             System.out.println(user.getPhone());
             }
             */
+            }
         }
     }
 }
