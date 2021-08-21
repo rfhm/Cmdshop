5423bcfd629a820ab542aa31e21124ee765171fc
rfhm <1981897922@qq.com>
2021-08-21 12:25:09 +0800

    1、把Excel用户信息读取到user数组（读取Excel信息） 2、比对键盘输入的用户名、密码和user的用户名、密码（验证登录） 3、解决密码、电话格式错误的问题 4、修改读取Excel文件的路径方法

--- Cmdshop/src/Test.java
+++ Cmdshop/src/Test.java
@@ -1,2 +1,35 @@
+import java.io.File;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //File file=new File("D:\\JAVAlx\\shangcheng\\src\\users.xlsx");
+        InputStream in=Class.forName("Test").getResourceAsStream("/users.xlsx");
+        ReadExcel readExcel=new ReadExcel();
+        User[] users=readExcel.readExcel(in);
+
+        System.out.println("请输入用户名");
+        Scanner sc=new Scanner(System.in);
+        String username=sc.next();
+
+        System.out.println("请输入密码");
+        String password=sc.next();
+
+        for(User user:users){
+            if(username.equals(user.getUsername())&&password.equals(user.getPassword())){
+                System.out.println("登录成功");
+                break;
+            }else{
+                System.out.println("用户名或者密码错误");
+            }
+             /*for(User user:users){
+            System.out.println(user.getUsername());
+            System.out.println(user.getPassword());
+            System.out.println(user.getAddress());
+            System.out.println(user.getPhone());
+            }
+            */
+        }
+    }
 }
